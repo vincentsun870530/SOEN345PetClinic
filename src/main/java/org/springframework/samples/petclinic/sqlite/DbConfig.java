75c14ab1cb8cc26a0f9018872136ce0ecbee2e31
AznBoy00 <Lin_Kevin_1995@hotmail.com>
2019-03-21 22:21:21 -0400

    #27 tests shouldnt crash now with enforced sqlite profile

--- src/main/java/org/springframework/samples/petclinic/sqlite/DbConfig.java
+++ src/main/java/org/springframework/samples/petclinic/sqlite/DbConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.core.env.Environment;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
@@ -13,6 +14,8 @@ import javax.sql.DataSource;
 import java.util.Properties;
 
 @ComponentScan
+@Configuration
+@Profile("sqlite")
 public class DbConfig {
     @Autowired
     private Environment env;
