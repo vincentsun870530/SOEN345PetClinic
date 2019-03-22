fc2908f82def8729c0daa4d53b4d2e3c01901fa5
unknown <davebhardwaj@gmail.com>
2019-03-22 11:38:46 -0400

    Fixed bug changed database name to petclinic

--- src/main/resources/db/sqlite/forklift_step.sh
+++ src/main/resources/db/sqlite/forklift_step.sh
@@ -1,10 +1,15 @@
 #1.Get dump.sql from mySQL db
-mysqldump --complete-insert=TRUE --extended-insert=FALSE --protocol=TCP -t -h localhost -P 3306 -u root -p petclinic > dump.sql 
-
+echo "MySQLDUMP starting....."
+mysqldump --complete-insert=TRUE --extended-insert=FALSE --protocol=TCP -t -h localhost -P 3306 -u root --password=root petclinic > dump.sql 
+sleep 3
+echo "done!"
 #2.Copy data schema from mySQL to SQLite
-sqlite3 database.sqlite3 < schemaLite.sql
-
+echo "Adding schema"
+sqlite3 petclinic.sqlite3 < schemaLite.sql
+echo "Schema Added!"
+sleep 3
+echo "Populating DB"
 #3.write and translate dump.sql to SQLite
-cat dump.sql | perl sql2lite.pl | sqlite3 database.sqlite3
-
+cat dump.sql | perl sql2lite.pl | sqlite3 petclinic.sqlite3
+echo "Populated DB!"
 echo "SUCCESSFUL"
