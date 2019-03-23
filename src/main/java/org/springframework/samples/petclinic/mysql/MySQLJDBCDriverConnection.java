4d6d2f0dc917e57190d40fe6c738c6c1db9b56e2
Yefei Xue <felixyf0124@gmail.com>
2019-03-23 15:41:20 -0400

    #33  re-added mysql connection class

---
+++ src/main/java/org/springframework/samples/petclinic/mysql/MySQLJDBCDriverConnection.java
@@ -0,0 +1,61 @@
+package org.springframework.samples.petclinic.mysql;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class MySQLJDBCDriverConnection {
+
+    public Connection connect() {
+        String url = "jdbc:mysql://127.0.0.1:3306/petclinic";
+        String user = "root";
+        String password = "petclinic";
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection(url, user, password);
+            System.out.println("Connect to MySQL");
+        } catch(SQLException exception) {
+            System.out.println("MySQLJDBCDriverConnection/connect:"+exception);
+        };
+        return connection;
+    }
+
+    public ResultSet selectAll(String tableName) {
+        String query = "SELECT * FROM " + tableName;
+        ResultSet resultSet = null;
+        try {
+            Connection connection = connect();
+            Statement statement = connection.createStatement();
+            resultSet = statement.executeQuery(query);
+
+            // FOR TESTING PURPOSE, TO BE REMOVED
+            // int count = 1;
+            // while(resultSet.next()) {
+            //     System.out.println(count + "." + resultSet.getString("name"));
+            // }
+            //////////////////////////////////////
+
+        } catch (SQLException exception) {
+            System.out.println("MySQLJDBCDriverConnection/selectAll:"+exception);
+        }
+        return resultSet;
+    }
+
+    //select one item 
+    public ResultSet selectById(String tableName, int id){
+        String query = "SELECT * FROM " + tableName + " WHERE id = " + id;
+        ResultSet resultSet = null;
+        try  {
+            Connection connection = connect();
+            Statement stmt = connection.createStatement();
+            
+            resultSet = stmt.executeQuery(query);
+           
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return resultSet;
+    }
+}
