8ab7c9e055615e06df05ecde6b1133ef8cf6085d
vincent.sun870530@gmail.com <vincent.sun870530@gmail.com>
2019-03-22 23:09:34 -0400

    modify forklift scipt to fix sql 5.7

--- src/main/resources/db/sqlite/forklift_step.sh
+++ src/main/resources/db/sqlite/forklift_step.sh
@@ -1,6 +1,9 @@
 #1.Get dump.sql from mySQL db
 echo "MySQLDUMP starting....."
+#mysql 8.0 
 mysqldump --column-statistics=0 --protocol=TCP -t -h localhost -P 3306 -u root --password=root petclinic > dump.sql 
+#mysql 5.7
+#mysqldump --protocol=TCP -t -h localhost -P 3306 -u root --password=root petclinic > dump.sql
 sleep 3
 echo "done!"
 #2.Copy data schema from mySQL to SQLite
