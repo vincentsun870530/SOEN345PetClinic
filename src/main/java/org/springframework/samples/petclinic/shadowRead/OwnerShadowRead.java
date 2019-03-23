ee9dba7708f3ccd69e3a63dca0ddaee1c463f4e6
Nizar Belhassan <m.nizar.belhassan@gmail.com>
2019-03-23 18:03:19 -0400

    #35 Added shadow read method for Owner

---
+++ src/main/java/org/springframework/samples/petclinic/shadowRead/OwnerShadowRead.java
@@ -0,0 +1,80 @@
+package org.springframework.samples.petclinic.shadowRead;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import org.springframework.samples.petclinic.owner.Owner;
+import org.springframework.samples.petclinic.owner.OwnerRepository;
+import org.springframework.samples.petclinic.sqlite.SQLiteDBConnector;
+import org.springframework.scheduling.annotation.Async;
+
+public class OwnerShadowRead {
+		
+		private int readInconsistencies = 0;
+		
+		SQLiteDBConnector sqLiteDbConnector = new SQLiteDBConnector();
+		
+		@Async
+		public void shadowRead(OwnerRepository repo, int id) throws SQLException{
+
+			//Owner in old MySql database with the specified id
+			Owner owner = (Owner) repo.findById(id);
+
+			//Owner in SqLite db with the specified id
+			ResultSet result = sqLiteDbConnector.selectById("Owner", id);
+
+			//initialise variables
+			String firstName = null;
+			String lastName = null;
+			String address = null;
+			String city = null;
+			String telephone = null;
+			
+			//get the number of the column in the SqLite database
+			int first_name_col = result.findColumn("first_name");
+			int last_name_col = result.findColumn("last_name");
+			int address_col = result.findColumn("address");
+			int city_col = result.findColumn("city");
+			int telephone_col = result.findColumn("telephone");
+			
+			//Obtain the value of the row proper to the specified id
+			if(result != null) {
+				firstName = (String) result.getObject(first_name_col);
+				lastName = (String) result.getObject(last_name_col);
+				address = (String) result.getObject(address_col);
+				city = (String) result.getObject(city_col);
+				telephone = (String) result.getObject(telephone_col);
+			}
+
+			//checking that the values of the rows retrieved from both old and new databases are the same
+			//when the rows are different increment readConsistencies and print the inconsistency
+			if(!(owner.getFirstName().equals(firstName))){
+				ReadInconsistency(owner.getFirstName(), firstName);
+			}
+
+			if(!(owner.getLastName().equals(lastName))){
+				ReadInconsistency(owner.getLastName(), lastName);
+			}
+
+			if(!(owner.getAddress().equals(address))){
+				ReadInconsistency(owner.getAddress(), address);
+			}
+
+			if(!(owner.getCity().equals(city))){
+				ReadInconsistency(owner.getCity(), city);
+			}
+
+			if(!(owner.getTelephone().equals(telephone))){
+				ReadInconsistency(owner.getTelephone(), telephone);
+			}
+		}
+
+		//print read inconsistency
+		private void ReadInconsistency(Object oldRow, Object newRow) {
+			System.out.println("\n Old object in Mysql db does not match the new object in SqLite db" + "\n Old: " + oldRow	+ "\n New: " + newRow);
+			readInconsistencies++;
+		}
+		
+		public int getReadInconsistencies() {
+			return readInconsistencies;
+		}
+	}
+
