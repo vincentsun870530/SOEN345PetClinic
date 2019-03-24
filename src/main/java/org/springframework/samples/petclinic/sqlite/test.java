c09dbef4276f7dd48a341230cd31bd2683988e97
Yefei Xue <felixyf0124@gmail.com>
2019-03-23 22:02:56 -0400

    #30 fixed few bugs and naming
    PS: When retrieve date from ResultSet, do not use getDate() method.
    Instead, use getString()

--- src/main/java/org/springframework/samples/petclinic/sqlite/test.java
+++ src/main/java/org/springframework/samples/petclinic/sqlite/test.java
@@ -1,9 +1,31 @@
 package org.springframework.samples.petclinic.sqlite;
 
+import java.sql.ResultSet;
+
 public class test {
     public static void main(String[] args) {
 
-        SQLiteResultSet.getOwners(new SQLiteDBConnector().selectAll("owners"));
+        SQLiteResultSet.getVisits(new SQLiteDBConnector().selectAll("visits"));
+//        ResultSet rs = new SQLiteDBConnector().selectAll("pets");
+//
+//        try {
+//            while (rs.next()) {
+//                //String strDate = dateFormat.format(rs.getDate("birth_date"));
+//                System.out.println(rs.getInt("id") + "\t" +
+//                        rs.getString("name") + "\t" +
+//                        "\t" +
+//                        rs.getInt("type_id") + "\t" +
+//                        rs.getInt("owner_id"));
+//
+//
+//                System.out.println(rs.getString("birth_date"));
+//            }
+//        }  catch (Exception e) {
+//            System.out.println(e.getMessage());
+//        }
+//        try(r.next()) {
+//            System.out.print(r.getString());
+//        }
 
     }
 }
