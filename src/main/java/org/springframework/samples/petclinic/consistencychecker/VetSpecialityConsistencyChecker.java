52655841233fe9798d3768ba1b1dfeb930097b1c
unknown <davebhardwaj@gmail.com>
2019-03-24 23:24:29 -0400

    Added vetSpeciality consistency checker

--- src/main/java/org/springframework/samples/petclinic/consistencychecker/VetSpecialityConsistencyChecker.java
+++ src/main/java/org/springframework/samples/petclinic/consistencychecker/VetSpecialityConsistencyChecker.java
@@ -1,60 +1,58 @@
-//package org.springframework.samples.petclinic.consistencychecker;
-//
-//import org.springframework.samples.petclinic.sqlite.SQLiteDBConnector;
-//import org.springframework.samples.petclinic.vet.Specialty;
-//
-//import java.lang.reflect.Array;
-//import java.util.HashMap;
-//import java.util.List;
-//
-//public class VetSpecialityConsistencyChecker implements InConsistencyChecker{
-//    private List<Array> oldVetSpecialityData;
-//    private List<Array> newVetSpecialtyData;
-//
-//    public void setOldData(List<Array> oldVetSpecialityData){this.oldVetSpecialityData = oldVetSpecialityData;}
-//    public void setNewData(List<Array>  newVetSpecialtyData){this.newVetSpecialtyData = newVetSpecialtyData;}
-//
-//    public int consistencyChecker(){
+package org.springframework.samples.petclinic.consistencychecker;
+
+import org.springframework.samples.petclinic.sqlite.SQLiteDBConnector;
+import org.springframework.samples.petclinic.vet.Specialty;
+
+import java.lang.reflect.Array;
+import java.util.HashMap;
+import java.util.List;
+
+public class VetSpecialityConsistencyChecker implements InConsistencyChecker{
+    private List<int[]> oldVetSpecialityData;
+    private List<int[]> newVetSpecialtyData;
+
+    public void setOldData(List<int[]> oldVetSpecialityData){this.oldVetSpecialityData = oldVetSpecialityData;}
+    public void setNewData(List<int[]>  newVetSpecialtyData){this.newVetSpecialtyData = newVetSpecialtyData;}
+
+    public int consistencyChecker(){
 //        Array oldVetSpeciality = ();
 //        Array newVetSpecialty;
-//        int atID;
-//        int inconsistency = 0;
-//        for(int index=0; index < oldVetSpecialityData.size(); index++) {
-//            oldVetSpeciality = oldVetSpecialityData.get(index);
-//            newVetSpecialty = newVetSpecialtyData.get(index);
-//            //need the number of columns (use hardcoded number or dynamically check the number of columns)
-//            //for PetType,  columns 2
-//            if(oldVetSpeciality.toString() != newVetSpecialty.toString()) {
-//                atID = oldVetSpeciality.;
-//                checkNewAndOldData(atID, oldVetSpeciality.getName(), newVetSpecialty.getName(),"name");
-//                inconsistency++;
-//            }
-//        }
-//        return inconsistency;
-//    }
-//
-//    public double calculateConsistencyChecker(int inconsistency) {
-//        int sizeOfSpecialty = oldSpecialityData.size();
-//        double consistency = (1 - (inconsistency/sizeOfSpecialty))*100;
-//        return Double.parseDouble(String.format("%.2f", consistency));
-//    }
-//
-//    private void checkNewAndOldData(int id, String oldData, String newData, String columnName){
-//        checkNewAndOldData(id,oldData,newData,columnName,"specialties");
-//    }
-//
-//    private void checkNewAndOldData(int id, String oldData, String newData, String columnName, String tableName) {
-//        if(oldData != newData) {
-//            printViolationMessage(id, oldData, newData);
-//            new SQLiteDBConnector().updateById(tableName,columnName, oldData, id);
-//        }
-//    }
-//
-//    public void printViolationMessage(int id, String oldData, String newData) {
-//        System.out.println("The row " + id + " on the new database," +
-//                " does not match: New(" + newData +
-//                " is not equal to Old(" + oldData);
-//    }
-//
-//
-//}
+        int atID;
+        int inconsistency = 0;
+        for(int index=0; index < oldVetSpecialityData.size(); index++) {
+            int[] oldSpec = oldVetSpecialityData.get(index);
+            int[] newSpec = newVetSpecialtyData.get(index);
+            if(!(oldSpec[0] == newSpec[0] && oldSpec[1] == newSpec[1])) {
+                atID = oldSpec[0];
+                //checkNewAndOldData(atID, oldVetSpeciality.getName(), newVetSpecialty.getName(),"name");
+                inconsistency++;
+            }
+        }
+        return inconsistency;
+    }
+
+    public double calculateConsistencyChecker(int inconsistency) {
+        int sizeOfSpecialty = oldVetSpecialityData.size();
+        double consistency = (1 - (inconsistency/sizeOfSpecialty))*100;
+        return Double.parseDouble(String.format("%.2f", consistency));
+    }
+
+    private void checkNewAndOldData(int id, String oldData, String newData, String columnName){
+        checkNewAndOldData(id,oldData,newData,columnName,"specialties");
+    }
+
+    private void checkNewAndOldData(int id, String oldData, String newData, String columnName, String tableName) {
+        if(oldData != newData) {
+            printViolationMessage(id, oldData, newData);
+            new SQLiteDBConnector().updateById(tableName,columnName, oldData, id);
+        }
+    }
+
+    public void printViolationMessage(int id, String oldData, String newData) {
+        System.out.println("The row " + id + " on the new database," +
+                " does not match: New(" + newData +
+                " is not equal to Old(" + oldData);
+    }
+
+
+}
