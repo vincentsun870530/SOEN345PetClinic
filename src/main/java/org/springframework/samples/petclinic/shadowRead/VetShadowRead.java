5533a760fecfc31999da491ae4884fddaf660b73
Wei He <hewei196126@gmail.com>
2019-03-27 01:00:46 -0400

    #35 add SQLiteVetHelper

---
+++ src/main/java/org/springframework/samples/petclinic/shadowRead/VetShadowRead.java
@@ -0,0 +1,4 @@
+package org.springframework.samples.petclinic.shadowRead;
+
+public class VetShadowRead {
+}
---
+++ src/main/java/org/springframework/samples/petclinic/sqlite/SQLiteVetHelper.java
@@ -0,0 +1,4 @@
+package org.springframework.samples.petclinic.sqlite;
+
+public class SQLiteVetHelper {
+}
