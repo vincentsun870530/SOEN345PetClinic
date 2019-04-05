dd4b22fd8acc572e6f7b68276228be3f3077a92f
unknown <davebhardwaj@gmail.com>
2019-04-05 15:01:26 -0400

    added feature toggle for welcome

--- src/main/java/org/springframework/samples/petclinic/system/WelcomeController.java
+++ src/main/java/org/springframework/samples/petclinic/system/WelcomeController.java
@@ -1,14 +1,26 @@
 package org.springframework.samples.petclinic.system;
 
 
+import org.springframework.samples.petclinic.FeatureToggles.RandomToggle;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.samples.petclinic.FeatureToggles.FeatureToggles;
+import java.util.concurrent.ThreadLocalRandom;
 
 @Controller
 class WelcomeController {
 
     @GetMapping("/")
     public String welcome() {
-        return "welcome";
+        RandomToggle rndToggle = new RandomToggle();
+        FeatureToggles.isEnabledLegacyWelcomePage = rndToggle.randomToggle(0.25f);
+        if(FeatureToggles.isEnabledLegacyWelcomePage == false) {
+            System.out.println("Running welcome V2");
+            return "welcome-v2";
+        }
+        else {
+            System.out.println("Running welcome V1");
+            return "welcome";
+        }
     }
 }
