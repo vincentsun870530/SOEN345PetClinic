c3cba3a56a2895ab1e981068ea4003e066f13b7c
WolfOfTheNorth <davebhardwaj@gmail.com>
2019-04-09 19:17:35 -0400

    analytics for time spent on welcome pahe

--- src/main/java/org/springframework/samples/petclinic/system/WelcomeController.java
+++ src/main/java/org/springframework/samples/petclinic/system/WelcomeController.java
@@ -2,6 +2,7 @@ package org.springframework.samples.petclinic.system;
 
 
 import org.springframework.samples.petclinic.FeatureToggles.RandomToggle;
+import org.springframework.samples.petclinic.FeatureToggles.timeAnalytics;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.samples.petclinic.FeatureToggles.FeatureToggles;
@@ -9,28 +10,33 @@ import org.springframework.samples.petclinic.FeatureToggles.FeatureToggles;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.time.Instant;
+
 @Controller
 class WelcomeController {
 
-    private static Logger analytics = LogManager.getLogger("Analytics");
+    private static Logger analytics = LogManager.getLogger("Welcome Toggle");
 
     @GetMapping("/")
     public String welcome() {
 
         RandomToggle rndToggle = new RandomToggle();
         // Legacy Mode 75% of the time V2 25% of the timeS
-        FeatureToggles.isEnabledLegacyWelcomePage = rndToggle.randomToggle(0.25f);
+        FeatureToggles.isEnabledLegacyWelcomePage = rndToggle.randomToggle(0.99f);
         return toggleWelcome();
     }
 
     private String toggleWelcome() {
+        //Start Time stamp to test ms spent on welcome page
+        timeAnalytics.resetTimeAnalystics();
+        timeAnalytics.startTime = System.nanoTime();
+
         if(FeatureToggles.isEnabledLegacyWelcomePage == false) {
-            analytics.info("Version 2 of welcome page started ");
+            analytics.info("Welcome Page Version 2");
             return "welcome-v2";
         }
         else {
-            analytics.info("Version 1 of welcome page started ");
-
+            analytics.info("Welcome Page Version 1");
             return "welcome";
         }
     }
