24e98820548b06ce3d57afac6584baafb346b4cf
Nizar Belhassan <m.nizar.belhassan@gmail.com>
2019-04-10 22:58:15 -0400

    #65 Implemented test that generates fake data for delete owner

---
+++ src/test/java/org/springframework/samples/petclinic/owner/GenerateOwnerDataTest.java
@@ -0,0 +1,27 @@
+package org.springframework.samples.petclinic.owner;
+
+import org.apache.commons.lang3.RandomUtils;
+import org.junit.Test;
+import org.springframework.samples.petclinic.ABTest.DeleteOwnerBtnHelper;
+import org.springframework.samples.petclinic.FeatureToggles.FeatureToggles;
+
+public class GenerateOwnerDataTest {
+
+    @Test
+    public void generateOwnerDataTest() {
+
+        int count = 0;
+        FeatureToggles.isEnableDeleteOwner = true;
+
+        while (count < 1000) {
+            int rnd = RandomUtils.nextInt(0, 2);
+            if (rnd == 0 && FeatureToggles.isEnableDeleteOwnerRandom) {
+                DeleteOwnerBtnHelper.countDeleteOwnerBtnOne();
+            }
+            else if (rnd == 1 && FeatureToggles.isEnableDeleteOwner){
+                DeleteOwnerBtnHelper.countDeleteOwnerBtnTwo();
+            }
+            count++;
+        }
+    }
+}
