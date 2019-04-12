bcf4bd610a2cb5346aaa8bbed8b385f5c6403ebf
AznBoy00 <Lin_Kevin_1995@hotmail.com>
2019-04-12 17:11:17 -0400

    #77 Added A/B test log generator unit test.

---
+++ src/test/java/org/springframework/samples/petclinic/featureToggle/welcomePageTest.java
@@ -0,0 +1,37 @@
+package org.springframework.samples.petclinic.featureToggle;
+
+import org.apache.logging.log4j.LogManager;
+import org.junit.Test;
+import org.springframework.samples.petclinic.FeatureToggles.FeatureToggles;
+import org.springframework.samples.petclinic.FeatureToggles.RandomToggle;
+
+import java.util.Random;
+
+
+public class welcomePageTest {
+
+    private static org.apache.logging.log4j.Logger timeLogAnalytics = LogManager.getLogger("WelcomeFeature");
+
+
+    @Test
+    public void testWelcomePage() {
+        int count =1000;
+        int low = 1000;
+        int high = 2000;
+
+        FeatureToggles.isEnabledLegacyWelcomePage = true;
+        RandomToggle rndToggle = new RandomToggle();
+        FeatureToggles.welcomePageToggle = rndToggle.randomToggle(0.30f);
+
+        Random r = new Random();
+        while(count != 0) {
+            int result = r.nextInt(high-low) + low;
+            timeLogAnalytics.info("Elapsed Time (ms) : " + result + " Legacy Welcome : " + FeatureToggles.welcomePageToggle);
+            count--;
+        }
+
+        
+    }
+
+
+}
